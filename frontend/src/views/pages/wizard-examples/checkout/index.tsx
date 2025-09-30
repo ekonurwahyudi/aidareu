0e3d2a3bcdc564f4eeef43397eb3251a4bef12bc
Eko Wahyudi <generasic@gmail.com>
2025-09-30 15:52:02 +0700

    update konfirmasi order

--- frontend/src/views/pages/wizard-examples/checkout/index.tsx
+++ frontend/src/views/pages/wizard-examples/checkout/index.tsx
@@ -55,7 +55,7 @@ const steps = [
     )
   },
   {
-    title: 'Confirmation',
+    title: 'Konfirmasi Order',
     icon: (
       <svg id='wizardConfirm' xmlns='http://www.w3.org/2000/svg' width='60' height='60' viewBox='0 0 60 60'>
         <g>
